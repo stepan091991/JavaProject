5e1ac7012f2483c63d82de02d1c02c394ad23c10
stepan091991 <stepan.fedorov00@mail.ru>
2026-04-01 22:11:13 +0500

    Tests[Physics OK]

--- src/tests/TestGame.java
+++ src/tests/TestGame.java
@@ -2,6 +2,7 @@ package tests;
 
 import core.*;
 import systems.ParticleSystem;
+import systems.PhysicsSystem;
 import utils.GameBuilder;
 import java.awt.*;
 
@@ -9,6 +10,7 @@ import static tests.TestGame.*;
 
 public class TestGame {
     static ParticleSystem particles;
+    static PhysicsSystem physics;
     static Sprite player;
     public static void main(String[] args) {
         // Создаем игру с помощью GameBuilder
@@ -22,6 +24,7 @@ public class TestGame {
         GamePanel panel = builder.build();
         GameWorld world = builder.getWorld();
         InputHandler input = builder.getInput();
+        physics = builder.getPhysics();
 
         // Создаем и добавляем простой объект
         SimpleObject obj = new SimpleObject(400, 300);
@@ -29,6 +32,19 @@ public class TestGame {
         player.resize(128,128);
         obj.setSprite(player);
         world.addObject(obj);
+
+        FallingCube cube = new FallingCube(600, 200);
+        world.addObject(cube);
+
+        FallingCube cube2 = new FallingCube(600, 50);
+        world.addObject(cube2);
+
+        // Регистрируем кубик в физической системе
+        physics.registerBody(cube);
+        physics.registerBody(cube2);
+
+        // Настраиваем гравитацию
+        physics.setGravity(0.2, 0);
 
         particles = new ParticleSystem();
 
@@ -61,6 +77,7 @@ class SimpleObject extends GameObject {
             if (input.isPressed("EXIT")) System.exit(0);
         }
         sprite.setPosition(x, y);
+        physics.update();
 
         // Границы
         x = Math.max(0, Math.min(750, x));
@@ -87,5 +104,38 @@ class SimpleObject extends GameObject {
         g.setFont(new Font("Arial", Font.BOLD, 16));
         g.drawString("Move with WASD/Arrows", 20, 50);
         g.drawString("Press ESC to exit", 20, 80);
+    }
+}
+
+class FallingCube extends GameObject {
+
+    public FallingCube(int x, int y) {
+        super(x, y, 50, 50);
+    }
+
+    @Override
+    public void update(InputHandler input) {
+        // Простые границы (чтобы кубик не упал за экран)
+        if (y + height >= 550) {
+            y = 550;
+        }
+
+        // Ограничение по X
+        x = Math.max(0, Math.min(750, x));
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        // Рисуем кубик
+        g.setColor(Color.CYAN);
+        g.fillRect(x, y, width, height);
+        g.setColor(Color.BLUE);
+        g.drawRect(x, y, width, height);
+
+        // Отображаем информацию
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.drawString("Position Y: " + y, 20, 70);
     }
 }
