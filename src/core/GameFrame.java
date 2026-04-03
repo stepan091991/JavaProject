2ef35803737e16e530b2cb124ac99c88bafadd26
stepan091991 <stepan.fedorov00@mail.ru>
2026-04-03 20:07:06 +0500

    Update GameFrame class(added title param)

--- src/core/GameFrame.java
+++ src/core/GameFrame.java
@@ -4,8 +4,8 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 
-    public GameFrame(GamePanel panel) {
-        setTitle("Modular Game");
+    public GameFrame(GamePanel panel, String title) {
+        setTitle(title);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         add(panel);
